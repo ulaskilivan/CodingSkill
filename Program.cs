ec3d2d7151058be652ccb7bfdad5f763439a1594
Ulaş <ulas.klvn@gmail.com>
2024-04-16 15:52:28 +0300

    Two Sum

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Program
+{
+    public static int[] TwoSum(int []nums,int target)
+    {
+        int[] newArray = new int[2];
+        for (int i = 0; i < nums.Length-1; i++)
+            
+        {
+            if(nums[i] + nums[i + 1] == target)
+            {
+                nums[0] = nums[i];
+                nums[1] = nums[i + 1];
+                break;
+                
+            }
+        }
+        return nums;
+    }
+    public static void Main(String[] args)
+    {
+        int[] numbers=TwoSum(new int[]{4,3,7,9,2,1},16);
+
+        Console.WriteLine(numbers[0]+" "+numbers[1]);
+    }
+}
